4a1f233e16f64034bd051d71a6eb2687f43c1bad
Chang chongsu <widenety@gmial.com>
2023-11-21 11:54:41 +0900

    test 수정7

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. #6
+          Edit <code>src/App.js</code> and save to reload. #7
         </p>
         <a
           className="App-link"
