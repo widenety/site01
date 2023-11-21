7caf60a92abd7c13d8094a8064a09ea9e2088af4
Chang chongsu <widenety@gmial.com>
2023-11-21 23:30:10 +0900

    레이아웃 컨텐츠 구분

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import logo from './logo.svg';
+import './layout.css';
 import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. #7
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		/*
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>Edit <code>src/App.js</code> and save to reload. #8</p>
+				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Learn React</a>
+			</header>
+		</div>
+		*/
+		<div id="doc">
+			<div className="opening">
+				<div className="msgBox">
+					<span className="t1">Widenety Style</span>
+					<span className="t2">web resources preview</span>
+				</div>
+			</div>
+		</div>
+	);
 }
-
 export default App;
