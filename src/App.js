a75a21f4459bb0ce715c9d603279508f0556bc10
Chang chongsu <widenety@gmial.com>
2023-11-22 00:53:19 +0900

    컨텐츠 레이아웃 스타일링 마크업 분리1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
+import './layout.css';
 import './App.css';
 function App() {
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>Edit <code>src/App.js</code> and save to reload. #8</p>
-				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Learn React</a>
-			</header>
+		<div className="doc">
+			<div className="opening">
+				<div className="msgBox">
+					<span className="t1">Widenety Style</span>
+					<span className="t2">web resources preview</span>
+				</div>
+			</div>
 		</div>
 	);
 }
