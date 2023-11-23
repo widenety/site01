636c2b6d954557e04917209ac91b7680cd597612
Chang chongsu <widenety@gmial.com>
2023-11-23 13:40:45 +0900

    디자인 보완, react-router-com 설치

--- src/App.js
+++ src/App.js
@@ -1,16 +1,50 @@
 import logo from './logo.svg';
 import './layout.css';
 import './App.css';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
 function App() {
 	return (
 		<div className="doc">
 			<div className="opening">
-				<div className="msgBox">
+				<h1 className="boxTitle">
 					<span className="t1">Widenety Style</span>
 					<span className="t2">web resources preview</span>
+				</h1>
+				<div className="boxLink">
+					<BrowserRouter>
+						<nav>
+							<ul id="test">
+								<li>
+									<Link to="/">Home</Link>
+								</li>
+								<li>
+									<Link to="/mypage">Mypage</Link>
+								</li>
+								<li>
+									<Link to="/dashboard">Dashboard</Link>
+								</li>
+							</ul>
+						</nav>
+						<Routes>
+							<Route path='/' element={<Home />} />
+							<Route path='/mypage' element={<MyPage />} />
+							<Route path='/dashboard' element={<Dashboard />} />
+						</Routes>
+					</BrowserRouter>
 				</div>
 			</div>
 		</div>
 	);
 }
 export default App;
+
+function Home() {
+	return <h1>Home</h1>
+}
+function MyPage() {
+	return <h1>Mypage</h1>
+}
+function Dashboard() {
+	return <h1>Dashboard</h1>
+}
