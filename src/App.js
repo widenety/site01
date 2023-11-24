b230b7d88d1edde663c30941ccb6865d25d3bba4
Chang chongsu <widenety@gmial.com>
2023-11-24 10:23:04 +0900

    사이트코드 역할 링크추가

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import './common.css';
 import './layout.css';
 import './App.css';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
@@ -7,34 +8,34 @@ function App() {
 	return (
 		<div className="doc">
 			<BrowserRouter>
-			<div className="opening">
-				<h1 className="boxTitle">
-					<span className="t1">Widenety Style</span>
-					<span className="t2">web resources preview</span>
-				</h1>
-				<div className="boxLink">
-					<nav>
-						<ul id="test">
-							<li>
-								<Link to="/">Home</Link>
-							</li>
-							<li>
-								<Link to="/mypage">Mypage</Link>
-							</li>
-							<li>
-								<Link to="/dashboard">Dashboard</Link>
-							</li>
-						</ul>
-					</nav>
+				<div className="opening">
+					<h1 className="boxTitle">
+						<span className="t1">Widenety Style</span>
+						<span className="t2">web resources preview</span>
+					</h1>
+					<div className="boxLink">
+						<nav>
+							<ul id="test">
+								<li>
+									<Link to="/site01/">Home</Link>
+								</li>
+								<li>
+									<Link to="/site01/mypage">Mypage</Link>
+								</li>
+								<li>
+									<Link to="/site01/dashboard">Dashboard</Link>
+								</li>
+							</ul>
+						</nav>
+					</div>
 				</div>
-			</div>
-			<div className="contents">
-				<Routes>
-					<Route path='/' element={<Home />} />
-					<Route path='/mypage' element={<MyPage />} />
-					<Route path='/dashboard' element={<Dashboard />} />
-				</Routes>
-			</div>
+				<div className="contents">
+					<Routes>
+						<Route path='/site01/' element={<Home />} />
+						<Route path='/site01/mypage' element={<MyPage />} />
+						<Route path='/site01/dashboard' element={<Dashboard />} />
+					</Routes>
+				</div>
 			</BrowserRouter>
 		</div>
 	);
