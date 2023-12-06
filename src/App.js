cb89ce722198536e3c534c777a5526bea7aa455d
Chang chongsu <widenety@gmial.com>
2023-12-06 12:46:57 +0900

    파이어베이스(파이어스토어) 연동 및 테스트 완료

--- src/App.js
+++ src/App.js
@@ -2,7 +2,53 @@ import logo from './logo.svg';
 import './common.css';
 import './layout.css';
 import './App.css';
+
+// ** 리액트 Router
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+//import { collection, getDocs } from 'firebase/firestore'
+//import { db } from 'firebase/firebase.config'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// ** 데이터 읽기
+import { getFirestore } from "firebase/firestore";
+import { collection, getDocs } from "firebase/firestore";
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+	apiKey: "AIzaSyCKHs1FIC5B0RRgLv0hWmDpHsCZ5VpfleE",
+	authDomain: "widenety-style-3c0b9.firebaseapp.com",
+	projectId: "widenety-style-3c0b9",
+	storageBucket: "widenety-style-3c0b9.appspot.com",
+	messagingSenderId: "43443741112",
+	appId: "1:43443741112:web:64dad5f131a98b2e202f4d",
+	measurementId: "G-XNW0MKRNCY"
+};
+const app = initializeApp(firebaseConfig);	// Initialize Firebase
+const db = getFirestore(app);				// Initialize Cloud Firestore and get a reference to the service
+const querySnapshot = await getDocs( collection( db, "clients" ) );
+const querySnapshotArray = [];
+let querySnapshotIdxTmp = null;
+async function getAllData() {
+	querySnapshot.forEach(( doc ) => {
+		let data = doc.data();
+		/* idx 중복검사 않함. 아이템 생성시 방지.
+		if ( doc.data().idx == querySnapshotIdxTmp ) {		// ** console.log( "중복존재" );
+		} else {											// ** console.log( "중복없음" );
+			querySnapshotArray.push( data );
+		}
+		querySnapshotIdxTmp = doc.data().idx;
+		// */
+		querySnapshotArray.push( data );
+	});
+	console.log( querySnapshotArray );
+}
+
+// ** 본체
 function App() {
 	return (
 		<div className="doc">
@@ -55,11 +101,14 @@ function App() {
 		</div>
 	);
 }
+
+// ** 타이틀태그 업데이트
 function setSiteTitle( txt ) {
 	const siteTitle = document.querySelector("title").dataset.titleString;
 	document.querySelector("title").innerText = siteTitle + " > " + txt;
 }
 function Home() {
+	getAllData();
 	setSiteTitle( "Home" );
 	return <h1>Home</h1>
 }
